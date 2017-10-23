fd7c8d3270d6eb599045a5d02f430c150eb4c595
Nick Bontrager <nick@nickbontrager.com>
2017-10-23 17:47:16 -0500

    added gerber files

--- CeilingStringPull/CeilingStringPull_attiny85/CeilingStringPull_attiny85.ino
+++ CeilingStringPull/CeilingStringPull_attiny85/CeilingStringPull_attiny85.ino
@@ -1,20 +1,23 @@
-//written for the attiny85 chip and easydriver v4.4
-
 #include <AccelStepper.h>
 
 // Define a stepper and the pins it will use
 AccelStepper stepper(AccelStepper::DRIVER, 1, 0);
+int motionpin = 2;
+int motionstatus = 0;
 
-long pos = 90000; //approx 6.5 feet, 200 steps/rev
+long pos = 90000; //approx 6.5 feet 90000
 
 void setup()
 { 
   stepper.setMaxSpeed(75000);
   stepper.setAcceleration(10000);
+  //pinMode(motionpin, INPUT);
+  randomSeed(analogRead(A0)); 
 }
 
 void loop()
 {
+  //motionstatus = analogRead(motionpin);
   if (stepper.distanceToGo() == 0)
   { int delaytime = int(random(1000,8000));
     delay(delaytime); //hold at the top
@@ -23,7 +26,7 @@ void loop()
     int maxspeedx = int(random(8000,25000));
     stepper.setMaxSpeed(maxspeedx);
     pos = -pos;
-    stepper.moveTo(pos);\
+    stepper.moveTo(pos);
   }
   stepper.run();
 }
--- LampSmoke/lampsmoke_attiny84/lampsmoke_attiny84.ino
+++ LampSmoke/lampsmoke_attiny84/lampsmoke_attiny84.ino
@@ -1,72 +1,58 @@
-#define NR_OF_LIGHTS 3
-int pins[NR_OF_LIGHTS] = {6, 7, 8};
+int redPin = 10;
+int greenPin = 9;
+int bluePin = 6;
+int randmin = 10;
+int randmax = 150;
 
-int values[NR_OF_LIGHTS];
-int steps[NR_OF_LIGHTS];
+//uncomment this line if using a Common Anode LED
+//#define COMMON_ANODE
 
-#define NR_OF_FADESTEPS 4
-int fadesteps[NR_OF_FADESTEPS] = { 192, 128, 64, 0 };
-int fade_delay = 300; // millisec
-int fade_cycles = 1000;
-
-int smokePin = 9;
-int smokeState = LOW;
-long previousMillis = 0; 
-long OnTime = 5000;
-long OffTime = 60*1000;  
-
-void setup() {
- randomSeed(analogRead(0));
- pinMode(smokePin,OUTPUT);
- for (int i = 0; i < NR_OF_LIGHTS; i++) {
-   values[i] = (int)random(230) + 13; // start values between 'max min' and 'min max'
-   steps[i] = (int)random(4) + 1; // steps between 1 and 4
- }
+void setup()
+{
+  pinMode(redPin, OUTPUT);
+  pinMode(greenPin, OUTPUT);
+  pinMode(bluePin, OUTPUT);
+  randomSeed(analogRead(0));  
 }
 
-void loop() {
- unsigned long currentMillis = millis();
-
-  if((smokeState == HIGH) && (currentMillis - previousMillis >= OnTime))
-  {
-    smokeState = LOW;  // Turn it off
-    OffTime = random((60*1000),(30*60*1000));
-    
-    previousMillis = currentMillis;  // Remember the time
-    digitalWrite(smokePin, smokeState);  // Update the actual LED
+void loop()
+{ int randselect = random(6);
+  switch (randselect){
+    case 0:
+      setColor(255, 0, 0);  // red
+      delay(random(randmin,randmax));
+    break;
+    case 1:
+      setColor(0, 255, 0);  // green
+      delay(random(randmin,randmax));
+    break;
+    case 2:
+      setColor(0, 0, 255);  // blue
+      delay(random(randmin,randmax));
+    break;
+    case 3:
+      setColor(255, 255, 0);  // yellow
+      delay(random(randmin,randmax));
+    break;
+    case 4:
+      setColor(80, 0, 80);  // purple
+      delay(random(randmin,randmax));
+    break;
+    case 5:
+      setColor(0, 255, 255);  // aqua
+      delay(random(randmin,randmax));
+    break;
   }
-  else if ((smokeState == LOW) && (currentMillis - previousMillis >= OffTime))
-  {
-    smokeState = HIGH;  // turn it on
-    OnTime = random(5000,10000);
-    previousMillis = currentMillis;   // Remember the time
-    digitalWrite(smokePin, smokeState);   // Update the actual LED
-  }
-    
- for (int j = 0; j < fade_cycles; j++) {
-   for (int i = 0; i < NR_OF_LIGHTS; i++) {
-     fadingLight(i);  
-   }
-   delay(fade_delay);
- }
 }
 
-void fadingLight(int i) {
- 
- int minvalue = (NR_OF_FADESTEPS * abs(steps[i])) + 1;
- int maxvalue = 255 - minvalue;
-
- int fs = NR_OF_FADESTEPS;
- for (int j = 0; fs > 0; fs--, j++) {
-   if (values[i] > fadesteps[j]) {
-       break;
-   }
- }
- values[i] += fs * steps[i];
- 
- if (values[i] > maxvalue  ||  values[i] < minvalue) {
-     steps[i] *= -1;
- }
- 
- analogWrite(pins[i], values[i]);
+void setColor(int red, int green, int blue)
+{
+  #ifdef COMMON_ANODE
+    red = 255 - red;
+    green = 255 - green;
+    blue = 255 - blue;
+  #endif
+  analogWrite(redPin, red);
+  analogWrite(greenPin, green);
+  analogWrite(bluePin, blue);  
 }
